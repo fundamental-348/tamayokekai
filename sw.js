bf053a9a216ec172c96cac08c2c4ec0c1a15949c
fundamental-348 <kraksrafdijg020424@gmail.com>
2025-07-12 10:13:53 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'ball-game-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './images/icon-192x192.png',
+  './images/icon-512x512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // キャッシュがあればそれを使用
+        if (response) {
+          return response;
+        }
+        // キャッシュがなければネットワークから取得
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
